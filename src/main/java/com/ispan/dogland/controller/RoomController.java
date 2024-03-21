f8acf1898ec67478c43198c3cf52c3fa4997b3ab
Ruan <a50064xxx@gmail.com>
2024-03-21 14:15:52 +0800

    準備合併建成

--- src/main/java/com/ispan/dogland/controller/RoomController.java
+++ src/main/java/com/ispan/dogland/controller/RoomController.java
@@ -1,41 +1,41 @@
-package com.ispan.dogland.controller;
-
-import com.ispan.dogland.model.entity.Dog;
-import com.ispan.dogland.model.entity.Room;
-import com.ispan.dogland.service.RoomService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class RoomController {
-
-    @Autowired
-    private RoomService rService;
-
-
-//    訂單資訊
+//package com.ispan.dogland.controller;
+//
+//import com.ispan.dogland.model.entity.Dog;
+//
+//import com.ispan.dogland.service.RoomService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class RoomController {
+//
+//    @Autowired
+//    private RoomService rService;
+//
+//
+////    訂單資訊
+////    @GetMapping("/room")
+////    public List<List<String>> room() {
+////        List<List<String>> roomList = rService.room();
+////
+////        return roomList;
+////    }
+//
 //    @GetMapping("/room")
-//    public List<List<String>> room() {
-//        List<List<String>> roomList = rService.room();
+//    public List<Room> room() {
+//        List<Room> roomList = rService.room();
 //
 //        return roomList;
 //    }
-
-    @GetMapping("/room")
-    public List<Room> room() {
-        List<Room> roomList = rService.room();
-
-        return roomList;
-    }
-
-    @GetMapping("/dog")
-    public List<Dog> dog() {
-        List<Dog> dogList = rService.dogs();
-
-        return dogList;
-    }
-
-}
+//
+//    @GetMapping("/dog")
+//    public List<Dog> dog() {
+//        List<Dog> dogList = rService.dogs();
+//
+//        return dogList;
+//    }
+//
+//}

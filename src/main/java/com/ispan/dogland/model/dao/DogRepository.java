7e3f01771df1dd448abc956ca0efa82f47245c35
YUI <sappon77698@gmail.com>
2024-03-27 15:57:59 +0800

    ‼️‼️‼️‼️我動了dogrepository‼️‼️‼️‼️ 裡面新增List<Dog> findByUser(Users user);

--- src/main/java/com/ispan/dogland/model/dao/DogRepository.java
+++ src/main/java/com/ispan/dogland/model/dao/DogRepository.java
@@ -1,8 +1,13 @@
 package com.ispan.dogland.model.dao;
 
 import com.ispan.dogland.model.entity.Dog;
+import com.ispan.dogland.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface DogRepository extends JpaRepository<Dog, Integer> {
     Dog findByDogId(Integer dogId);
+
+    List<Dog> findByUser(Users user);
 }

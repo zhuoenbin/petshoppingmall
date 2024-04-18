0cff219387d46185a8eb8a88b8eb54cf7b16f76f
enbin <boy820408@gmail.com>
2024-04-18 09:46:14 +0800

    1130418

--- src/main/java/com/ispan/dogland/controller/ForumController.java
+++ src/main/java/com/ispan/dogland/controller/ForumController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/forum")
-@CrossOrigin(allowCredentials = "true", origins = { "http://localhost:5173/", "http://127.0.0.1:5173" })
+//@CrossOrigin(allowCredentials = "true", origins = { "http://localhost:5173/", "http://127.0.0.1:5173" })
 public class ForumController {
 
     @Autowired
